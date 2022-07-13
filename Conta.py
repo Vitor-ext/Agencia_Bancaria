dcf69b73d979ccc5762302f9048726233b74280d
Vitor de Jesus <santanav92@gmail.com>
2022-07-12 21:11:39 -0300

    Avanços Python

---
+++ Conta.py
@@ -0,0 +1,6 @@
+class Conta:
+    def __init__(self, titular, numero, saldo):
+        self.saldo = saldo
+        self.numero = numero
+        self.titular = titular
+
---
+++ main.py
@@ -0,0 +1,11 @@
+from Cliente import Cliente
+from Conta import Conta
+
+class Main:
+    pass
+
+client= Cliente("Joao", "1198989-8989")
+
+conta= Conta(client.nome, 5050, 102456)
+
+print("Titular: ",conta.titular,"\nConta: ",conta.numero, "\nSeu Saldo é: ", "R$",conta.saldo)
