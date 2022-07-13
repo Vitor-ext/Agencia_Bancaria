212a8460851291ba6db28ff12e5bfc7d6548ddca
Vitor de Jesus <santanav92@gmail.com>
2022-07-12 22:05:36 -0300

    Encapsulamento

--- Cliente.py
+++ Cliente.py
@@ -1,4 +1,12 @@
 class Cliente:
-    def __init__(self, nome, fone): # Passando Dados
-        self.nome = nome
-        self.telefone = fone
+    def __init__(self, nome, fone):
+        self._nome = nome
+        self._telefone = fone
+
+# Metodo Get
+    def get_nome(self):
+        return self._nome
+
+# Metodo Set
+    def get_nome(self, nome):
+        self._nome = nome
--- Conta.py
+++ Conta.py
@@ -1,6 +1,22 @@
 class Conta:
-    def __init__(self, titular, numero, saldo): # Passando Dados
-        self.saldo = saldo
-        self.numero = numero
-        self.titular = titular
+    def __init__(self, titular, numero, saldo):
+        self._saldo = saldo
+        self._numero = numero
+        self._titular = titular
 
+    @property
+    def saldo(self):
+        return self._saldo
+
+    def titular(self):
+        return self._titular
+
+    def numero(self):
+        return self._numero
+
+    @saldo.setter
+    def saldo(self, saldo):
+        if (saldo < 0):
+            print("Saldo não poder ser negativo")
+        else:
+            self._saldo = saldo
